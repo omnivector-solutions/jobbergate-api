92b7a04f86036b18d65228a3f3b5951d4d60de63
Stephen Keefauver <stephenkeefauver@Stephens-MacBook-Pro.local>
2020-06-11 11:42:32 -0700

    revert master and branch off init

--- jobbergate/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from jobbergate.models import Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ('id', 'author', 'text', 'created', 'updated')
