0e0605b68972f46464c67a1aa014765ff03792f6
Stephen Keefauver <stephenkeefauver@OmniMB.local>
2020-06-12 13:12:26 -0700

    post get working for job-scripts will replicate for job submission and application

--- apps/job_scripts/urls.py
+++ apps/job_scripts/urls.py
@@ -1,36 +1,7 @@
-from django.urls import path, include
-
+from django.urls import path
 from apps.job_scripts import views
-from rest_framework import routers
-
-
-
-
-router = routers.DefaultRouter()
-router.register(r'', views.JobScriptView)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    # JobScriptListOrCreateView will return a list of all of the objects on GET
-    # and allow the creation of a job-script on POST.
-    #
-    # /job-scripts GET - Returns the list of job-scripts.
-    #              POST - Allows to create a job-script.
-    #
-    #path(
-    #    '',
-    #    views.JobScriptListOrCreateView.as_view(),
-    #),
-
-    # JobScriptDetailView lets you perform CRUD operations on the
-    # the object.
-    #
-    # /job-scripts/<pk>/ GET - Get details about a single job-script.
-    #                    UPDATE - Update details about a job-script.
-    #                    DELETE - Delete a job-script.
-    #
-    #path(
-    #    '<int:pk>/',
-    #    views.JobScriptDetailView.as_view()
-    #),
-]
+    path('job-script/', views.JobScriptListView.as_view()),
+    path('job-script/<int:pk>/', views.JobScriptView.as_view())
+    ]
--- apps/job_scripts/views.py
+++ apps/job_scripts/views.py
@@ -2,7 +2,7 @@ from rest_framework import mixins, viewsets, permissions
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
+from rest_framework import generics
 
 from apps.job_scripts.models import JobScript
 from apps.job_scripts.serializers import JobScriptSerializer
@@ -17,43 +17,17 @@ class JobScriptView(mixins.ListModelMixin, viewsets.GenericViewSet):
         return Response({"some": "keyvalue"})
 
 
-class JobScriptListOrCreateView(APIView):
+class JobScriptListView(generics.ListCreateAPIView):
     """
-    JobScriptListOrCreateView will return a list of all of the objects on GET
-    and allow the creation of a job-script on POST.
-
-    /job-scripts GET - Returns the list of job-scripts.
-                 POST - Allows to create a job-script.
+    list view for 'job-script/'
     """
+    queryset = JobScript.objects.all()
+    serializer_class = JobScriptSerializer
 
-    permission_classes = [permissions.IsAuthenticated]
+class JobScriptView(generics.RetrieveUpdateDestroyAPIView):
+    '''
+    detail view for 'job-script/<int:pk>'
+    '''
     serializer_class = JobScriptSerializer
     queryset = JobScript.objects.all()
 
-    def get(self, request):
-        #        user = request.user
-        #        job_scripts = JobScript.objects.get(user=user)
-        #        serializer = JobScriptSerializer(data=job_scripts, many=True)
-        #
-        #        print(serializer.__dict__)
-        #        return Response(JSONRenderer().render(serializer.data))
-        pass
-
-    def post(self, request):
-        pass
-
-
-class JobScriptDetailView(APIView):
-    """Job script get, post, update, delete operations.
-    * Requires token authentication.
-    endpoint: job-scripts/<pk>/
-    """
-
-    def get(self, request):
-        pass
-
-    def put(self, request):
-        pass
-
-    def delete(self, request):
-        pass
