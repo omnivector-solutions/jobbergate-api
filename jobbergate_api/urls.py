36c28a814fdaef212f1de79ab10c05e5721e4dde
Stephen Keefauver <stephenkeefauver@ip-192-168-0-100.us-west-2.compute.internal>
2020-06-29 08:08:34 -0700

    crud operations working deployed

--- jobbergate_api/urls.py
+++ jobbergate_api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 from rest_framework_jwt.views import obtain_jwt_token
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.job_scripts.urls')),
