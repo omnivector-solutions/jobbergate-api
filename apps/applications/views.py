146a5595e494b3f6a98e04faedb7bab0dc8cb283
Stephen Keefauver <stephenkeefauver@OmniMB.local>
2020-07-17 13:46:46 -0700

    API updates to support managing JS, rendering template, accepting param_file and others

--- apps/applications/views.py
+++ apps/applications/views.py
@@ -1,9 +1,15 @@
-from rest_framework import mixins, viewsets, permissions
+import os
 
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import status
+from rest_framework.parsers import FileUploadParser
+from rest_framework.exceptions import ParseError
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+
+import boto3
 
 from apps.applications.models import Application
 from apps.applications.serializers import ApplicationSerializer
@@ -15,11 +21,32 @@ class ApplicationListView(generics.ListCreateAPIView):
     """
     queryset = Application.objects.all()
     serializer_class = ApplicationSerializer
+    bucket = boto3.resource('s3').Bucket('omnivector-misc')
 
     def delete(self, request, Application_pk, format=None):
         application = Application.objects.get(id=Application_pk)
         application.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def post(self, request, format=None):
+        data = request.data
+        parser_class = (FileUploadParser,)
+        if 'upload_file' not in request.data:
+            raise ParseError("Empty content")
+        tar_file = data['upload_file']
+
+        filename = data['application_location'].split("/")[-1]
+
+        path = default_storage.save(filename, ContentFile(tar_file.read()))
+        self.bucket.upload_file(
+            os.path.join(settings.MEDIA_ROOT, path),
+            data['application_location'])
+
+        serializer = ApplicationSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
 
 class ApplicationView(generics.RetrieveUpdateDestroyAPIView):
@@ -31,8 +58,10 @@ class ApplicationView(generics.RetrieveUpdateDestroyAPIView):
 
     def put(self, request, pk, format=None):
         application = Application.objects.get(id=pk)
+
         data = request.data
         serializer = ApplicationSerializer(instance=application, data=data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
--- apps/job_scripts/views.py
+++ apps/job_scripts/views.py
@@ -1,11 +1,22 @@
-from rest_framework import mixins, viewsets, permissions
+import os
+import json
+import ast
+import io
 
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import status
+from rest_framework.parsers import FileUploadParser
+from rest_framework.exceptions import ParseError
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.conf import settings
+from jinja2 import Template
 
+import boto3
+import tarfile
+
+from apps.applications.models import Application
 from apps.job_scripts.models import JobScript
 from apps.job_scripts.serializers import JobScriptSerializer
 
@@ -16,19 +27,55 @@ class JobScriptListView(generics.ListCreateAPIView):
     """
     queryset = JobScript.objects.all()
     serializer_class = JobScriptSerializer
+    bucket = boto3.resource('s3').Bucket('omnivector-misc')
+
+    def post(self, request, format=None):
+        data = request.data
+        parser_class = (FileUploadParser,)
+        if 'upload_file' not in request.data:
+            raise ParseError("Empty content")
+        param_file = data['upload_file'].read()
+        dict_str = param_file.decode("UTF-8")
+        param_dict = ast.literal_eval(dict_str)
+
+        application = Application.objects.get(id=data['application'])
+        print(application.application_location)
+
+        print(param_dict)
+        obj = self.bucket.Object(application.application_location)
+        buf = io.BytesIO(obj.get()["Body"].read())  # reads whole gz file into memory
+        tar = tarfile.open(fileobj=buf)
+        # use "tar" as a regular TarFile object
+        for member in tar.getmembers():
+            print(member.name)
+            if ".j2" in member.name:
+                contentfobj = tar.extractfile(member)
+                template_file = contentfobj.read().decode("utf-8")
+
+            # f = tar.extractfile(member)
+            # print(f.)
+
+
+        template = Template(template_file)
+
+        # TODO Identify this file not hard code once working
+        rendered_js = template.render(param_dict=param_dict)
+        print(f"type rendered_js {type(rendered_js)}")
+        data['job_script_data_as_string'] = rendered_js
+        # outfile = open(os.path.join(application_path, f"{application_name}.sh"), "w")
+        # outfile.write(rendered_js)
+        # outfile.close()
+
+        serializer = JobScriptSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
 
     def delete(self, request, JobScript_pk, format=None):
         jobscript = JobScript.objects.get(id=JobScript_pk)
         jobscript.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-    # def put(self, request, JobScript_pk, format=None):
-    #     jobscript = JobScript.objects.get(pk=JobScript_pk)
-    #     data = request.data
-    #     serializer = JobScriptSerializer(instance=jobscript, data=data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data)
 
 
 class JobScriptView(generics.RetrieveUpdateDestroyAPIView):
