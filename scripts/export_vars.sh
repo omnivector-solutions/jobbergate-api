6cbdcca8e63a30d7d1c1b931a851dee4ef76a63b
Stephen Keefauver <stephenkeefauver@ip-192-168-0-101.us-west-2.compute.internal>
2020-07-21 16:01:46 -0700

    update export_vars.sh with stage

--- scripts/export_vars.sh
+++ scripts/export_vars.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+stage=$1
+
 export LAMBDA_TASK_ROOT="rats"
-export DATABASE_NAME=`npx serverless dbName | awk '{print $3}'`
-export DATABASE_USER=`npx serverless dbUser | awk '{print $3}'`
-export DATABASE_PORT=`npx serverless dbPort | awk '{print $3}'`
-export DATABASE_PASS=`npx serverless dbPass | awk '{print $3}'`
-export DATABASE_HOST=`npx serverless dbHost | awk '{print $3}'`
-export CLOUDFRONT_DOMAIN=`npx serverless domainInfo | awk '{print $3}'`
+export DATABASE_NAME=`npx serverless dbName --stage $stage | awk '{print $3}'`
+export DATABASE_USER=`npx serverless dbUser --stage $stage | awk '{print $3}'`
+export DATABASE_PORT=`npx serverless dbPort --stage $stage | awk '{print $3}'`
+export DATABASE_PASS=`npx serverless dbPass --stage $stage | awk '{print $3}'`
+export DATABASE_HOST=`npx serverless dbHost --stage $stage | awk '{print $3}'`
+export CLOUDFRONT_DOMAIN=`npx serverless domainInfo --stage $stage | awk '{print $3}'`
