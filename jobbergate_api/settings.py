d4e1a33e5ff580820f2c0e578754932d1ba4abe2
jamesbeedy <jamesbeedy@gmail.com>
2020-08-25 03:49:15 +0000

    python3.6 -> python3.8, iamroles

--- jobbergate_api/settings.py
+++ jobbergate_api/settings.py
@@ -88,7 +88,7 @@ if IS_OFFLINE:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['DATABASE_NAME'],
             'USER': os.environ['DATABASE_USER'],
             'PASSWORD': os.environ['DATABASE_PASS'],
