b3ed58b515bd131ea7a863343c3b4f60d60cd2e9
Stephen Keefauver <stephenkeefauver@OmniMB.local>
2020-08-30 07:25:45 -0700

    update-applictaion working

--- jobbergate_api/settings.py
+++ jobbergate_api/settings.py
@@ -9,7 +9,7 @@ S3_BUCKET = 'jobbbergate-api-staging-resources'
 
 S3_BASE_PATH = 'jobbergate-resources'
 
-TAR_NAME = "jobbergate.tar.gz"
+TAR_NAME = "/tmp/jobbergate.tar.gz"
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
